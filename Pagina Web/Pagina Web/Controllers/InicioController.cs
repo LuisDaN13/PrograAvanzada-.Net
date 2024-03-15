e34ba7e80abb85e1a543772049470c34fd26ac67
LuisDaN13 <109930322+LuisDaN13@users.noreply.github.com>
2024-03-14 21:35:22 -0600

    Commit Semana No.3

---
+++ Pagina Web/Pagina Web/Controllers/InicioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pagina_Web.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult PantallaPrincipal()
+        {
+            return View();
+        }
+    }
+}
